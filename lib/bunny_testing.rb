c9b567c33525db8fb242f2295ba343d0bcd58cf4
Vitaly Shlyaga <vitaly@shlyaga.ru>
2020-02-12 13:55:28 +0300

    initial commit

---
+++ lib/bunny_testing.rb
@@ -0,0 +1,6 @@
+require "bunny_testing/version"
+
+module BunnyTesting
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bunny_testing/version.rb
@@ -0,0 +1,3 @@
+module BunnyTesting
+  VERSION = "0.1.0"
+end
